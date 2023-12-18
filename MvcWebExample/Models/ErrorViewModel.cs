dd53331ca6113dc2e583fcf258347c1a2e049b2e
Georgios Dimopoulos <georgdimopoulos@outlook.com.gr>
2023-12-18 20:58:13 +0100

    Add project files.

---
+++ MvcWebExample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcWebExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
